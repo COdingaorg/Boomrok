007acf1ae6f9b317627cfdc8986466bda232d4d7
Codingaorg <calemasanga@gmail.com>
2021-11-20 17:14:13 +0300

    modifies models to add relation ships

--- boomrok_app/models.py
+++ boomrok_app/models.py
@@ -9,39 +9,40 @@ class User_Subscribers(models.Model):
     '''
     first_name = models.CharField(max_length=15)
     last_name = models.CharField(max_length=15)
-    email = models.CharField()
-    phone_number = models.CharField()
+    email = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    #subscription_status = models.TextChoices()
+
+class Movie(models.Model):
+    '''
+    Main movie, marking active on display means it displays on homepage
+    '''
+    movie_number = models.CharField(unique=True, max_length=100)
+    title = models.CharField(unique = True, max_length=50)
+    sub_title = models.TextField()
+    poster = models.ImageField(upload_to = 'Posters/')
+    active_on_display = models.BooleanField(default=False, unique=True)
+    movie_info = models.TextField()
+    subscribers = models.ManyToManyField(to=User_Subscribers)
+    movie_item = models.TextField()
 
 class Cast_Members(models.Model):
     '''
     Actors on a given movie
     '''
     image = models.ImageField(upload_to = 'Cast_Members/')
-    first_name = models.CharField()
-    last_name = models.CharField()
-    screen_name = models.CharField()
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    screen_name = models.CharField(max_length=35)
+    role = models.TextField()
+    movie = models.ForeignKey(Movie, on_delete=CASCADE)
 
 class Trailer(models.Model):
     '''
     Trailer models, related to movie
     '''
-    trailer_title = models.CharField()
-    trailer_item = models.CharField()
-
-class Movie(models.Model):
-    '''
-    Main movie, marking active on display means it displays on homepage
-    '''
-    movie_number = models.CharField(unique=True)
-    title = models.CharField(unique = True, max_length=50)
-    sub_title = models.CharField()
-    poster = models.ImageField(upload_to = 'Posters/')
-    active_on_display = models.BooleanField(default=False, unique=True)
-    movie_info = models.TextField()
-    cast = models.ManyToManyField(to=Cast_Members)
-    subscribers = models.ManyToManyField(to=User_Subscribers)
-    movie_item = models.CharField()
-    trailer = models.ForeignKey(Trailer, on_delete= CASCADE)
-
+    trailer_title = models.TextField()
+    trailer_item = models.TextField()
+    movie = models.ForeignKey(Movie, on_delete=CASCADE)
 
 
