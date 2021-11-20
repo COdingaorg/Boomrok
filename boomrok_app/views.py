571fc37182935a12ddd3d5dbe309141896999d32
Codingaorg <calemasanga@gmail.com>
2021-11-20 17:19:45 +0300

    adds view function to index, template

--- boomrok_app/views.py
+++ boomrok_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+site_name = 'Boomrok'
+def index(request):
+    '''
+    view function rendering index page
+    '''
+    context={
+        site_name : 'title',
+    }
+    return(request, context, 'user/home.html')
